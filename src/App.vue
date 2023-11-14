e50f1c3c349317a60481d906c37bb7b7c8698b68
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-14 15:48:22 +0600

    Feat: add 'NavigationBar.vue' component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
     import { ref } from 'vue';
+    import NavigationBar from './components/NavigationBar.vue';
     import UserLogin from './components/UserLogin.vue';
-    import QueryResult from './components/QueryResult.vue';
 
-    const activeLink = ref('')
     const token = ref('')
-    const query = ref('')
 
-    function logout() {
+    function removeToken() {
         localStorage.removeItem('token');
         token.value = localStorage.getItem('token')
     }
@@ -16,58 +14,15 @@
         token.value = localStorage.getItem('token')
     }
 
-    function activateLink(link) {
-        activeLink.value = link
-        query.value = link
-    }
-
 </script>
 
 <template>
     <div v-if="token">
-        <ul>
-            <li><a href="#" @click="activateLink('client')" :class="{ 'active': activeLink === 'client' }">Client</a></li>
-            <li><a href="#" @click="activateLink('user')" :class="{ 'active': activeLink === 'user' }">User</a></li>
-            <li class="nav-right"><a href="#" @click="logout">Logout</a></li>
-        </ul>
-        <div v-if="query">
-            <QueryResult @auth-required="logout" />
-        </div>
+        <NavigationBar @logout="removeToken" />
     </div>
     <div v-else>
         <UserLogin @login-success="updateToken" />
     </div>
 </template>
 
-<style scoped>
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #333;
-    }
-
-    li {
-        float: left;
-        border-right: 1px solid #bbb;
-
-    }
-
-    li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
-
-    .active {
-        background-color: rgb(3, 170, 114);
-        color: black;
-    }
-
-    li.nav-right {
-        float: right;
-    }
-</style>
+<style scoped></style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,55 @@
+<script setup>
+    import { ref } from 'vue'
+    import QueryResult from './QueryResult.vue';
+
+    const activeLink = ref('')
+
+    function linkClicked(link) {
+        activeLink.value = link
+    }
+
+</script>
+
+<template>
+    <ul>
+        <li><a href="#" @click="linkClicked('add')" :class="{ 'active': activeLink === 'add' }">Add</a></li>
+        <li><a href="#" @click="linkClicked('search')" :class="{ 'active': activeLink === 'search' }">Search</a></li>
+        <li class="nav-right"><a href="#" @click="$emit('logout')">Logout</a></li>
+    </ul>
+    <div v-if="activeLink === 'search'">
+        <QueryResult />
+    </div>
+</template>
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+        border-right: 1px solid #bbb;
+
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    .active {
+        background-color: rgb(3, 170, 114);
+        color: black;
+    }
+
+    li.nav-right {
+        float: right;
+    }
+</style>
