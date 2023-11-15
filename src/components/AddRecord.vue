88c98080e89765d1b7286e05915bcd3e6138dbad
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-15 14:29:47 +0600

    Feat: add form and function to add new client

--- src/components/AddRecord.vue
+++ src/components/AddRecord.vue
@@ -1,10 +1,46 @@
 <script setup>
     import { ref } from 'vue'
 
+    const emit = defineEmits(['auth-required'])
     const linkName = ref('')
+    const message = ref('')
+    const apiUrl = 'http://127.0.0.1:8000/'
+    const clientName = ref('')
 
     function clickedLink(link) {
         linkName.value = link
+        message.value = ''
+        clientName.value = ''
+    }
+
+    async function addClient() {
+        const apiEndpoint = apiUrl + 'clients/add'
+
+        try {
+            const response = await fetch(apiEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    'name': clientName.value,
+                })
+            });
+
+            if (response.status === 200) {
+                message.value = `Client ${clientName.value} added successfully`
+            } else if (response.status === 401) {
+                console.log('Need authentication')
+                emit('auth-required')
+            } else if (response.status === 400) {
+                message.value = 'Client exists'
+            }
+            else {
+                console.log('Unknown error')
+            }
+        } catch (error) {
+            console.error('Network or API error:', error);
+        }
     }
 
 </script>
@@ -15,6 +51,14 @@
         <li><button @click="clickedLink('client')">Add Client</button></li>
         <li><button @click="clickedLink('contact')">Add Contact</button></li>
     </ul>
+
+    <div v-if="linkName === 'client'">
+        <div v-if="message">{{ message }}</div>
+        <form @submit.prevent="addClient">
+            <input v-model="clientName" type="text" placeholder="client name">
+            <button type="submit">Add</button>
+        </form>
+    </div>
 </template>
 
 <style scoped>
