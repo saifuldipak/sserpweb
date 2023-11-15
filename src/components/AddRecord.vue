6e5d587ac670cd074c121b53a1e5aae23254ae86
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-15 12:02:54 +0600

    Feat: add component 'AddRecord'

---
+++ src/components/AddRecord.vue
@@ -0,0 +1,52 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const linkName = ref('')
+
+    function clickedLink(link) {
+        linkName.value = link
+    }
+
+</script>
+
+<template>
+    <ul>
+        <li><button @click="clickedLink('service')">Add Service</button></li>
+        <li><button @click="clickedLink('client')">Add Client</button></li>
+        <li><button @click="clickedLink('contact')">Add Contact</button></li>
+    </ul>
+</template>
+
+<style scoped>
+    ul {
+        list-style: none;
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        margin-top: 10px;
+
+    }
+
+    li {
+        flex: 0 0 auto;
+        margin-right: 10px;
+    }
+
+    button {
+        background-color: rgb(249, 234, 215);
+        padding: 8px;
+        border: 0px;
+    }
+
+    button:hover {
+        background-color: rgb(231, 207, 177);
+
+
+    }
+
+    @media(max-width: 768px) {
+        ul li {
+            margin-right: 5px;
+        }
+    }
+</style>
