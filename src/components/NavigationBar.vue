0baf15fa3ee0146bd4c0477aec15b5272f2c8ad0
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-19 12:37:16 +0600

    Feat: change navigation bar design

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,8 +1,5 @@
 <script setup>
     import { ref } from 'vue'
-    import QueryResult from './QueryResult.vue';
-    import AddRecord from './AddRecord.vue'
-
     const activeLink = ref('')
 
     function linkClicked(link) {
@@ -12,48 +9,107 @@
 </script>
 
 <template>
-    <ul>
-        <li><a href="#" @click="linkClicked('add')" :class="{ 'active': activeLink === 'add' }">Add</a></li>
-        <li><a href="#" @click="linkClicked('search')" :class="{ 'active': activeLink === 'search' }">Search</a></li>
-        <li class="nav-right"><a href="#" @click="$emits('logout')">Logout</a></li>
-    </ul>
-    <div v-if="activeLink === 'search'">
+    <nav>
+        <ul>
+            <li class="dropdown">
+                <img src="./icons/menu_button_2.png">
+                <div class="dropdown-content">
+                    <a href="#">Search</a>
+                    <a href="#">Add Client</a>
+                </div>
+            </li>
+            <li class="logout"><img src="./icons/Profile-Avatar.png">saiful</li>
+        </ul>
+    </nav>
+    <!-- <nav>
+        <ul>
+            <li class="dropdown">
+                <a href="#">Menu</a>
+            </li>
+            <div class="dropdown-content">
+                <a href="#">Web Design</a>
+                <a href="#">Graphic Design</a>
+                <a href="#">SEO</a>
+            </div>
+
+            <li><a href="#" @click="linkClicked('search')" :class="{ 'active': activeLink === 'search' }">Search</a></li>
+            <li class="nav-right"><a href="#" @click="$emits('logout')">Logout</a></li>
+        </ul>
+    </nav> -->
+    <!-- <div v-if="activeLink === 'search'">
         <QueryResult @auth-required="$emits('logout')" />
     </div>
     <div v-else-if="activeLink === 'add'">
         <AddRecord @auth-required="$emits('logout')" />
-    </div>
+    </div> -->
 </template>
 
 <style scoped>
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #333;
+    nav {
+        background-color: #2a2a2a;
+        color: #fff;
+        text-align: center;
     }
 
-    li {
-        float: left;
-        border-right: 1px solid #bbb;
+    nav ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
 
+    }
+
+    nav li {
+        padding: 15px;
+        background-color: rgb(26, 182, 86);
+    }
+
+    li img {
+        height: 35px;
+        width: 35px;
     }
 
     li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
+        color: #f3f5f3;
         text-decoration: none;
     }
 
-    .active {
-        background-color: rgb(3, 170, 114);
-        color: black;
+    li.logout {
+        display: flex;
+        align-items: center;
+        background-color: #2a2a2a;
     }
 
-    li.nav-right {
-        float: right;
+    /* Dropdown styles */
+    .dropdown {
+        position: right;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #333;
+        min-width: 160px;
+        box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+        text-align: left;
+    }
+
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
+
+    .dropdown-content a {
+        color: #fff;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+        font-size: medium;
+    }
+
+    .dropdown-content a:hover {
+        background-color: #555;
     }
 </style>
