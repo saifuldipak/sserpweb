48098920aaec9a6769b1d0d028294640313657e5
Saiful Islam <saiful.kbtel@gmail.com>
2023-12-06 11:36:47 +0600

    Feat: toggle visibity of client list and client details

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,32 +1,53 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, watch } from 'vue'
 
+    const hideList = ref(false)
     const showClient = ref('')
+    const hideDetails = ref(true)
 
     const props = defineProps({
         clients: Array
+    })
+
+    function toggleListAndDetails(clientName = '') {
+        showClient.value = clientName
+
+        if (hideList.value === false && hideDetails.value === true) {
+            hideList.value = true
+            hideDetails.value = false
+        }
+        else {
+            hideList.value = false
+            hideDetails.value = true
+        }
+    }
+
+    watch(props, () => {
+        hideList.value = false
+        hideDetails.value = true
     })
 
 </script>
 <template>
     <!-- search result presentation -->
     <div v-if="props.clients.length > 0">
-        <table>
+        <table :class="{ 'client-list-hide': hideList }">
             <tr class="table-header">
                 <th>Id</th>
                 <th>Name</th>
             </tr>
             <tr v-for=" client  in  props.clients " :key="client.id">
                 <td><a href="#">{{ client.id }}</a></td>
-                <td><a href="#" @click="showClient = client.name">{{ client.name }}</a> </td>
+                <td><a href="#" @click="toggleListAndDetails(client.name)">{{ client.name }}</a> </td>
             </tr>
         </table>
     </div>
-    <div v-if="showClient">
+    <div v-if="showClient" :class="{ 'client-details-hide': hideDetails }">
+        <button @click="toggleListAndDetails">Show search result</button>
         <ul v-for="client in props.clients" :key="client.id">
             <div v-if="client.name === showClient">
                 <li>Client Id: {{ client.id }}</li>
-                <li>Client Name: {{ clientname }}</li>
+                <li>Client Name: {{ client.name }}</li>
                 <li>Client Type: {{ client.client_type.name }}</li>
                 <li>Contacts:
                     <ul v-for=" contact  in  client.contacts " :key="contact.id">
@@ -42,12 +63,16 @@
 </template>
 
 <style scoped>
-    /*search result presentation*/
+    /*client list*/
     table {
         width: 75%;
         margin: auto;
         border-radius: 5px;
         box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+    }
+
+    .client-list-hide {
+        display: none;
     }
 
     th {
@@ -62,15 +87,8 @@
         background-color: rgb(161, 250, 165);
     }
 
-    .error-message-container {
-        text-align: center;
-    }
-
-    .error-message {
-        color: red;
-        text-align: center;
-        background-color: whitesmoke;
-        padding: 3px;
-
+    /*client details*/
+    .client-details-hide {
+        display: none;
     }
 </style>
