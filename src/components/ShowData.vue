4b911f370a8ac332bbf44430a58a83e5e00914d5
Saiful Islam <saiful.kbtel@gmail.com>
2023-12-10 15:44:25 +0600

    Fix: generalize template for all kinds of data

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -6,12 +6,13 @@
 </script>
 
 <template>
-    <div v-if="props.dataType === 'Clients'">
-        <table v-if="props.data" class="data-table">
-            <tr v-for="client in props.data" :key="client.id" class="client-list">
-                <td>{{ client.id }}</td>
-                <td>{{ client.name }}</td>
-                <td>{{ client.client_type.name }}</td>
+    <div v-if="props.data">
+        <table class="data-table">
+            <tr v-for="item in props.data" :key="item.id" class="data">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td v-if="props.dataType === 'Clients'">{{ item.client_type.name }}</td>
+                <td v-else-if="props.dataType === 'Contacts'"> {{ item.clients.name }}</td>
             </tr>
         </table>
     </div>
