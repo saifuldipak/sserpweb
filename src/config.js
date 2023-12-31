7442f2a407a1603ffb2cc557ecfa9e2034b9e48f
Saiful Islam <saiful.kbtel@gmail.com>
2023-12-31 18:01:20 +0600

    Feat: add function 'createRequestBody'

--- src/config.js
+++ src/config.js
@@ -1,1 +1,30 @@
 export const API_URL = "http://127.0.0.1:8000/";
+
+export const createRequestBody = function (method, body = "") {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        console.log("JWT not found in local storage");
+        emit("auth-required");
+    }
+
+    let request;
+    if (body) {
+        request = {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(body),
+        };
+    } else {
+        request = {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+    }
+    return request;
+};
