1d697e0e5a11951486c05ec6d18fbfa4db1bc9ce
Saiful Islam <saiful.kbtel@gmail.com>
2024-01-11 12:53:50 +0600

    Fix: replace 'searchRecord' & 'selectClient' with 'searchSuggestions' & 'selectSuggestions' respectively

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -2,7 +2,6 @@
     import { ref } from 'vue'
     import { createApiUrl, createRequest, callApi } from '@/functions.js'
 
-    const itemName = ref('')
     const clientTypeId = ref(0)
     const apiMessage = ref('')
     const apiError = ref('')
@@ -10,15 +9,15 @@
     const clientId = ref(0)
     const serviceType = ref('')
     const servicePoint = ref('')
-    const popId = ref(0)
     const extraInfo = ref('')
-    const bandwidth = ref(0)
+    const bandwidth = ref()
     const clientName = ref('')
-    const clientList = ref([])
     const vendorName = ref('')
     const vendorType = ref()
-    const itemList = ref([])
     const popName = ref('')
+    const popList = ref([])
+    const clientList = ref([])
+    const popId = ref()
 
     const props = defineProps({
         viewName: String,
@@ -55,7 +54,7 @@
         }
     }
 
-    const searchRecord = async (searchItem) => {
+    /* const searchRecord = async (searchItem) => {
         apiMessage.value = ''
         apiError.value = ''
         let apiEndpoint
@@ -76,12 +75,62 @@
                 apiError.value = error.message
             }
         }
+    } */
+    const searchSuggestions = async (view) => {
+        apiMessage.value = ''
+        apiError.value = ''
+
+        let itemName, itemList
+        if (view === 'Clients') {
+            itemName = clientName.value
+        }
+        else if (view === 'Pops') {
+            itemName = popName.value
+        }
+
+        if (itemName.length > 2) {
+            const apiEndpoint = createApiUrl({ view: view, action: 'search', searchString: itemName })
+            const request = createRequest('GET')
+            const { code, response, error } = await callApi(apiEndpoint, request)
+
+            apiMessage.value = ''
+            apiError.value = ''
+            if (code === 200) {
+                itemList = response
+            }
+            else if (code !== 200) {
+                apiMessage.value = response.detail
+            }
+            else {
+                apiError.value = error.message
+            }
+        }
+
+        if (view === 'Clients') {
+            clientList.value = itemList
+        }
+        else if (view === 'Pops') {
+            popList.value = itemList
+        }
     }
 
-    const selectClient = (id, name) => {
+    /* const selectClient = (id, name) => {
         clientId.value = id
         clientName.value = name
         clientList.value = []
+    } */
+
+    const selectSuggestion = (view, id, name) => {
+        if (view === 'Clients') {
+            clientId.value = id
+            clientName.value = name
+            clientList.value = []
+        }
+        else if (view === 'Pops') {
+            popId.value = id
+            popName.value = name
+            popList.value = []
+        }
     }
 
 </script>
@@ -90,44 +139,77 @@
     <div v-if="apiMessage">{{ apiMessage }}</div>
     <div v-if="apiError">{{ apiError }}</div>
     <form @submit.prevent="submitForm">
+        <!-- Clients -->
         <div v-if="props.viewName === 'Clients'">
             <input type="text" placeholder="client name" v-model="itemName">
             <select v-model="clientTypeId">
                 <option v-for="item in props.data" :value="item.id">{{ item.name }}</option>
             </select>
         </div>
+        <!--Vendors-->
         <div v-else-if="props.viewName === 'Vendors'">
             <input type="text" placeholder="vendor name" v-model="vendorName">
             <select v-model="vendorType">
                 <option v-for="item in props.data" :key="item.id" :value="item.name">{{ item.name }}</option>
             </select>
         </div>
-        <div v-else-if="props.viewName === 'Services'">
-            <input type="text" placeholder="Client name" v-model="itemName" @input="searchRecord('Clients')">
-            <ul v-if="clientName.length > 0 && !apiMessage && !apiError" class="suggestions">
+        <!-- Services -->
+        <!-- <div v-else-if="props.viewName === 'Services'" class="add-form">
+            <input class="client-name" type="text" placeholder="Client name" v-model="itemName"
+                @input="searchRecord('Clients')">
+            <ul v-if="itemName.length > 0 && !apiMessage && !apiError" class="suggestions">
                 <li class="suggestion" v-for="client in itemList" :key="client.id"
                     @click="selectClient(client.id, client.name)">{{
                         client.name }}
                 </li>
             </ul>
             <input type="text" placeholder="Service location" v-model="servicePoint">
-            <select v-model="serviceType">
-                <option v-for="item in props.data" :value="item.id">{{ item.name }}</option>
+            <select v-model="serviceType" class="select-box">
+                <option disabled value="">service type</option>
+                <option placeholder="service type" v-for="item in props.data" :value="item.id">{{ item.name }}</option>
             </select>
-            <input type="number" placeholder="bandwidth value in Mbps" v-model="bandwidth">
-            <input type="integer" placeholde="pop id" v-model="popId">
-            <input type="text" placeholder="extra info" v-model="extraInfo">
-        </div>
-        <div v-else-if="props.viewName === 'Pops'">
-            <input type="text" placeholder="Pop name" v-model="popName">
-            <input type="text" placeholder="Vendor name" v-model="itemName" @input="searchRecord('Vendors')">
-            <ul v-if="itemName.length > 0 && !apiMessage && !apiError" class="suggestions">
+            <input type="integer" placeholder="bandwidth value in Mbps" v-model="bandwidth">
+            <input type="text" placeholder="Pop name" v-model="popName" @input="searchRecord('Pops')">
+            <ul v-if="Name.length > 0 && !apiMessage && !apiError" class="suggestions">
                 <li class="suggestion" v-for="pop in itemList" :key="pop.id" @click="selectItem(pop.id, pop.name)">{{
                     item.name }}
                 </li>
             </ul>
+            <input type="text" placeholder="extra info" v-model="extraInfo">
+        </div> -->
 
-
+        <div v-else-if="props.viewName === 'Services'" class="add-form">
+            <input class="client-name" type="text" placeholder="Client name" v-model="clientName"
+                @input="searchSuggestions('Clients')">
+            <ul v-if="clientName.length > 0 && !apiMessage && !apiError" class="suggestions">
+                <li class="suggestion" v-for="client in clientList" :key="client.id"
+                    @click="selectSuggestion('Clients', client.id, client.name)">{{
+                        client.name }}
+                </li>
+            </ul>
+            <input type="text" placeholder="Service location" v-model="servicePoint">
+            <select v-model="serviceType" class="select-box">
+                <option disabled value="">service type</option>
+                <option placeholder="service type" v-for="item in props.data" :value="item.id">{{ item.name }}</option>
+            </select>
+            <input type="integer" placeholder="bandwidth value in Mbps" v-model="bandwidth">
+            <input type="text" placeholder="Pop name" v-model="popName" @input="searchSuggestions('Pops')">
+            <ul v-if="popName.length > 0 && !apiMessage && !apiError" class="suggestions">
+                <li class="suggestion" v-for="pop in popList" :key="pop.id"
+                    @click="selectSuggestion('Pops', pop.id, pop.name)">{{
+                        pop.name }}
+                </li>
+            </ul>
+            <input type="text" placeholder="extra info" v-model="extraInfo">
+        </div>
+        <!-- Pops -->
+        <div v-else-if="props.viewName === 'Pops'">
+            <input type="text" placeholder="Pop name" v-model="popName" @input="searchRecord('Pops')">
+            <ul v-if="popName.length > 0 && !apiMessage && !apiError" class="suggestions">
+                <li class="suggestion" v-for="pop in itemList" :key="pop.id" @click="selectItem(pop.id, pop.name)">{{
+                    item.name }}
+                </li>
+            </ul>
         </div>
         <div v-else-if="props.viewName === 'Service Types'">
             <input type="text" placeholder="service type name" v-model="itemName" required>
@@ -138,6 +220,10 @@
 </template>
 
 <style scoped>
+    .client-name {
+        position: relative;
+    }
+
     .suggestions {
         list-style-type: none;
         padding: 0;
@@ -156,7 +242,18 @@
 
     /* Ensure that the suggestions dropdown is displayed above other content */
     .suggestions {
-        position: absolute;
+        background-color: grey;
+        position: relative;
         z-index: 1000;
+    }
+
+    .add-form {
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .select-box option:disabled {
+        color: gray;
     }
 </style>
