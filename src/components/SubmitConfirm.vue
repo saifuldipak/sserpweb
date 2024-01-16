9ce4ef459feed76009daf0fefa6a1ba15940cdb7
Saiful Islam <saiful.kbtel@gmail.com>
2024-01-16 15:42:28 +0600

    Fix: change display style of 'SubmitConfirm' component

--- src/components/SubmitConfirm.vue
+++ src/components/SubmitConfirm.vue
@@ -22,12 +22,31 @@
 </script>
 
 <template>
-    <div class="modal" v-if="show">
+    <div class="confirm" v-if="show">
         <span v-if="props.actionName === 'add'">Are you sure you want to add?</span>
-        <span v-if="props.actionName === 'modify'">Are you sure you want to modify?</span>
-        <span v-if="props.actionName === 'delete'">Are you sure you want to delete?</span>
-        <button @click="confirm">Confirm</button>
-        <button @click="cancel">Cancel</button>
+        <div class="message" v-if="props.actionName === 'modify'">Are you sure you want to modify?</div>
+        <div class="message" v-if="props.actionName === 'delete'">Are you sure you want to delete?</div>
+        <div class="button-section">
+            <button @click="confirm">Confirm</button>
+            <button @click="cancel">Cancel</button>
+        </div>
     </div>
 </template>
   
+<style scoped>
+    .confirm {
+        display: flex;
+        flex-direction: column;
+        background-color: lightgoldenrodyellow;
+        text-align: center;
+    }
+
+    .message {
+        align-items: center;
+    }
+
+    .button-section {
+        display: flex;
+        justify-content: center;
+    }
+</style>
