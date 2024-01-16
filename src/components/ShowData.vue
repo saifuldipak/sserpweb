885c06a9ad0c250820761dbbe530d67023e070be
Saiful Islam <saiful.kbtel@gmail.com>
2024-01-16 11:49:18 +0600

    Fix: show data using <table> element

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { onMounted, ref } from 'vue'
+    import { ref } from 'vue'
     import ClientDetails from './ClientDetails.vue';
     import ServiceDetails from './ServiceDetails.vue';
 
@@ -11,40 +11,34 @@
     const itemDetails = ref()
 
     const emit = defineEmits(['modifyItem', 'deleteItem'])
-
-    /* onMounted(() => {
-        console.log(props.data, props.dataType)
-    }) */
 </script>
 
 <template>
-    <ul v-if="props.data" class="item-list" :class="{ 'item-list-hide': itemDetails }">
-        <div v-for="item in props.data" :key="item.id">
-            <li class="item">
-                <div v-if="props.viewName === 'Clients'">
-                    <a href="#" @click="itemDetails = item">{{ item.name }}</a>
-                    <span class="item-type">{{ item.client_type.name }}</span>
-                </div>
-                <div v-else-if="props.viewName === 'Services'">
-                    <a href="#" @click="itemDetails = item">{{ item.point }}</a>
-                    <span class="item-type"> {{ item.clients.name }}</span>
-                </div>
-                <div v-else-if="props.viewName === 'Service Types'">
-                    <span class="item-type">{{ item.name }}</span>
-                </div>
-                <div v-else-if="props.viewName === 'Vendors'">
-                    <span class="item-type">{{ item.name }}</span>
-                </div>
-                <div v-else-if="props.viewName === 'Pops'">
-                    <span class="item-type">{{ item.name }}</span>
-                </div>
-                <div class="button-section">
-                    <button class="modify" @click="$emit('modifyItem', item.id)">Modify</button>
-                    <button class="delete" @click="$emit('deleteItem', item.id)">Delete</button>
-                </div>
-            </li>
-        </div>
-    </ul>
+    <table v-if="props.data" :class="{ 'item-list-hide': itemDetails }">
+        <tr v-for="item in props.data" :key="item.id">
+            <td v-if="props.viewName === 'Clients'">
+                <a href="#" @click="itemDetails = item">{{ item.name }}</a>
+            </td>
+            <div v-else-if="props.viewName === 'Services'">
+                <a href="#" @click="itemDetails = item">{{ item.point }}</a>
+            </div>
+            <div v-else-if="props.viewName === 'Service Types'">
+                <a href="#" @click="itemDetails = item">{{ item.point }}</a>
+            </div>
+            <div v-else-if="props.viewName === 'Vendors'">
+                <a href="#" @click="itemDetails = item">{{ item.point }}</a>
+            </div>
+            <div v-else-if="props.viewName === 'Pops'">
+                <a href="#" @click="itemDetails = item">{{ item.point }}</a>
+            </div>
+            <td v-if="props.viewName === 'Clients'">{{ item.client_type.name }}</td>
+            <div v-else-if="props.viewName === 'Services'">{{ item.clients.name }}</div>
+            <td class="button-section">
+                <button class="modify" @click="$emit('modifyItem', item.id)">Modify</button>
+                <button class="delete" @click="$emit('deleteItem', item.id)">Delete</button>
+            </td>
+        </tr>
+    </table>
     <div v-if="itemDetails">
         <button @click="itemDetails = ''">X</button>
         <ClientDetails v-if="props.viewName === 'Clients'" :item="itemDetails" />
@@ -54,27 +48,24 @@
 
 <style scoped>
     .item-list {
-        list-style-type: none;
+        width: 50%;
+        display: flex;
+        justify-content: left;
     }
 
     .item-list-hide {
         display: none;
     }
 
-    .item-type {
-        margin-left: 50px;
-    }
-
-    .item {
-        display: flex;
+    td {
+        padding: 0;
+        padding-left: 10px;
+        padding-right: 10px;
     }
 
     .button-section {
-        margin-left: 10px;
-    }
-
-    .button-section button {
-        margin-left: 5px;
+        display: flex;
+        justify-content: space-around;
     }
 
     .modify {
