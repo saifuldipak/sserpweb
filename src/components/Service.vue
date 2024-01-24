837d9651dd2ed6aa9f0571c18862c053ae953e50
Saiful Islam <saiful.kbtel@gmail.com>
2024-01-24 11:48:43 +0600

    Fix: use 'SearchSuggestion' component and move notification handling to 'App' component

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,9 +1,9 @@
 <script setup>
-    import { onMounted, ref } from 'vue'
+    import { onMounted, ref, watchEffect } from 'vue'
     import { createApiUrl, createRequest } from '@/functions.js'
     import SubmitConfirm from './SubmitConfirm.vue';
     import SubmitButton from './SubmitButton.vue';
-    import Notification from './Notification.vue';
+    import SearchSuggestion from './SearchSuggestion.vue';
 
     const apiMessage = ref('')
     const apiError = ref('')
@@ -11,23 +11,29 @@
     const serviceTypeId = ref()
     const servicePoint = ref('')
     const extraInfo = ref('')
-    const bandwidth = ref()
+    const bandwidth = ref('')
     const clientName = ref('')
     const popName = ref('')
     const popList = ref([])
     const clientList = ref([])
     const popId = ref()
-    const serviceName = ref('')
-    const serviceList = ref([])
     const serviceId = ref()
     const serviceTypes = ref()
     const dialogVisible = ref(false)
-    const showDetails = ref(true)
+    const showDetails = ref(false)
     const showForm = ref(true)
     const message = ref('')
     const messageType = ref('')
-    const hideNotification = ref(false)
     const showButton = ref(true)
+    const showHeader = ref(true)
+    const clientData = ref({
+        id: '',
+        name: ''
+    })
+    const popData = ref({
+        id: '',
+        name: ''
+    })
 
     const props = defineProps({
         actionName: {
@@ -38,6 +44,8 @@
             type: Object,
         }
     })
+
+    const emit = defineEmits(['showNotification'])
 
     const hadChanged = () => {
         if (props.itemData.client_id !== clientId.value) {
@@ -62,13 +70,38 @@
         return false
     }
 
+    const checkInputs = () => {
+        if (!clientId.value || !servicePoint.value || !serviceTypeId.value || !bandwidth.value || !popId.value) {
+            return false
+        }
+
+        if (props.actionName === 'modify') {
+            if (!serviceId.value) {
+                return false
+            }
+        }
+
+        return true
+    }
+
     const submitForm = async () => {
+        if (props.actionName === 'add' || props.actionName === 'modify') {
+            const inputsCorrect = checkInputs()
+            if (!inputsCorrect) {
+                message.value = 'Need to give inputs to all fields'
+                messageType.value = 'Error'
+                emit('showNotification', message.value, messageType.value)
+                closeDialog()
+                return
+            }
+        }
         if (props.actionName === 'modify') {
             const dataModified = hadChanged()
             if (!dataModified) {
-                apiMessage.value = 'Nothing modified'
+                message.value = 'Nothing modified'
+                messageType.value = 'Warning'
+                emit('showNotification', message.value, messageType.value)
                 closeDialog()
-                hideNotification.value = false
                 return
             }
         }
@@ -120,13 +153,17 @@
                     message.value = 'Service deleted'
                 }
                 messageType.value = 'Info'
+                emit('showNotification', message.value, messageType.value)
+                showHeader.value = false
                 showDetails.value = false
                 showForm.value = false
             }
             else {
                 const data = await response.json()
-                message.value = data.detail
+                console.error(data)
+                message.value = 'Data validation error'
                 messageType.value = 'Error'
+                emit('showNotification', message.value, messageType.value)
             }
         }
         catch (error) {
@@ -139,59 +176,7 @@
         }
     }
 
-    const searchSuggestions = async (view) => {
-        apiMessage.value = ''
-        apiError.value = ''
-
-        let itemName
-        if (view === 'Clients') {
-            itemName = clientName.value
-        }
-        else if (view === 'Pops') {
-            itemName = popName.value
-        }
-        else if (view === 'Services') {
-            itemName = serviceName.value
-        }
-
-
-        let itemList
-        if (itemName.length > 2) {
-            const apiEndpoint = createApiUrl({ view: view, action: 'search', searchString: itemName })
-            const request = createRequest('GET')
-
-            apiMessage.value = ''
-            apiError.value = ''
-            try {
-                const response = await fetch(apiEndpoint, request)
-                if (response.status === 200) {
-                    itemList = await response.json()
-                }
-                else {
-                    const data = await response.json()
-                    message.value = data.detail
-                    messageType.value = 'Error'
-                }
-            }
-            catch (error) {
-                console.error(error)
-                message.value = error.message
-                messageType.value = 'Error'
-            }
-        }
-
-        if (view === 'Clients') {
-            clientList.value = itemList
-        }
-        else if (view === 'Pops') {
-            popList.value = itemList
-        }
-        else if (view === 'Services') {
-            serviceList.value = itemList
-        }
-    }
-
-    const selectSuggestion = (view, id, name) => {
+    const selectedSuggestion = (view, id, name) => {
         if (view === 'Clients') {
             clientId.value = id
             clientName.value = name
@@ -201,11 +186,6 @@
             popId.value = id
             popName.value = name
             popList.value = []
-        }
-        else if (view === 'Services') {
-            serviceId.value = id
-            serviceName.value = name
-            serviceList.value = []
         }
     }
 
@@ -231,15 +211,20 @@
             message.value = error.message
             messageType.value = 'Error'
         }
+    })
 
+    watchEffect(() => {
         if (props.actionName === 'modify' || props.actionName === 'delete') {
             serviceId.value = props.itemData.id
-            selectSuggestion('Clients', props.itemData.client_id, props.itemData.clients.name)
+            clientData.value.id = props.itemData.clients.id
+            clientData.value.name = props.itemData.clients.name
             servicePoint.value = props.itemData.point
             serviceTypeId.value = props.itemData.service_type_id
             bandwidth.value = props.itemData.bandwidth
-            selectSuggestion('Pops', props.itemData.pop_id, props.itemData.pops.name)
+            popData.value.id = props.itemData.pops.id
+            popData.value.name = props.itemData.pops.name
             extraInfo.value = props.itemData.extra_info
+            showDetails.value = true
         }
     })
 
@@ -253,11 +238,11 @@
 </script>
 
 <template>
-    <h3 v-if="props.actionName === 'add'">Add Service</h3>
-    <h3 v-else-if="props.actionName === 'modify'">Modify Service</h3>
-    <h3 v-else-if="props.actionName === 'delete'">Delete Service</h3>
-    <Notification v-if="message && !hideNotification" :message="message" :message-type="messageType"
-        @hide-notification="hideNotification = true" />
+    <div v-if="showHeader">
+        <h3 v-if="props.actionName === 'add'">Add Service</h3>
+        <h3 v-else-if="props.actionName === 'modify'">Modify Service</h3>
+        <h3 v-else-if="props.actionName === 'delete'">Delete Service</h3>
+    </div>
     <div class="details" v-if="showDetails">
         <span v-if="props.actionName === 'modify' || props.actionName === 'delete'">Service Id: {{ serviceId }}</span>
         <span v-if="props.actionName === 'delete'">Service Point: {{ servicePoint }}</span>
@@ -266,13 +251,10 @@
     <div class="data-form" v-if="showForm">
         <form @submit.prevent="handleSubmit">
             <div class='form-fields' v-if="props.actionName === 'add' || props.actionName === 'modify'">
-                <input class="client-name" type="text" placeholder="Client name" v-model="clientName"
-                    @input="searchSuggestions('Clients')">
-                <ul v-if="clientName.length > 0 && !apiMessage && !apiError" class="suggestions">
-                    <li class="suggestion" v-for="client in clientList" :key="client.id"
-                        @click="selectSuggestion('Clients', client.id, client.name)">{{ client.name }}
-                    </li>
-                </ul>
+                <SearchSuggestion v-if="props.actionName === 'add'" :view-name="'Clients'"
+                    @selected-item-id="selectedSuggestion" />
+                <SearchSuggestion v-else-if="props.actionName === 'modify'" :view-name="'Clients'" :item-data="clientData"
+                    @selected-item-id="selectedSuggestion" />
                 <input type="text" placeholder="Service location" v-model="servicePoint">
                 <select v-model="serviceTypeId" class="select-box">
                     <option disabled value="">service type</option>
@@ -280,13 +262,10 @@
                         serviceType.name }}</option>
                 </select>
                 <input type="integer" placeholder="bandwidth value in Mbps" v-model="bandwidth">
-                <input type="text" placeholder="Pop name" v-model="popName" @input="searchSuggestions('Pops')">
-                <ul v-if="popName.length > 0 && !apiMessage && !apiError" class="suggestions">
-                    <li class="suggestion" v-for="pop in popList" :key="pop.id"
-                        @click="selectSuggestion('Pops', pop.id, pop.name)">
-                        {{ pop.name }}
-                    </li>
-                </ul>
+                <SearchSuggestion v-if="props.actionName === 'add'" :view-name="'Pops'"
+                    @selected-item-id="selectedSuggestion" />
+                <SearchSuggestion v-else-if="props.actionName === 'modify'" :view-name="'Pops'" :item-data="popData"
+                    @selected-item-id="selectedSuggestion" />
                 <input type="text" placeholder="extra info" v-model="extraInfo">
             </div>
             <div v-if="showButton">
