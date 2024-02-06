d4f0ed515810e31df88b80fc6735a12832baacda
Saiful Islam <saiful.kbtel@gmail.com>
2024-02-06 17:58:15 +0600

    Fix: replace component 'ServiceForm' and 'ClientForm' with 'Form'

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,34 +1,95 @@
 <script setup>
-    import { ref, onMounted } from 'vue'
-    import { createApiUrl, createRequest, selectMethod, createBody } from '@/functions.js'
+    import { ref, onMounted, watchEffect } from 'vue'
+    import { createApiUrl, createRequest } from '@/functions.js'
     import SubmitConfirm from './SubmitConfirm.vue';
-    import ClientForm from './ClientForm.vue';
-    import ServiceForm from './ServiceForm.vue';
+    import Forms from './Forms.vue'
 
     const message = ref()
     const messageType = ref()
     const serviceTypes = ref([])
     const clientTypes = ref([])
     const dialogVisible = ref(false)
-    const serviceData = ref({
-        id: '',
-        client_id: '',
-        point: '',
-        service_type_id: '',
-        bandwidth: '',
-        pop_id: '',
-        extra_info: ''
-    })
-    const clientData = ref({
-        id: '',
-        name: '',
-        client_type_id: ''
-    })
     const notification = ref({
         message: '',
         type: ''
     })
+    const formData = ref({
+        client: {
+            name: '',
+            client_type_id: ''
+        },
+        service: {
+            client_id: '',
+            point: '',
+            service_type_id: '',
+            bandwidth: '',
+            pop_id: '',
+            extra_info: ''
+        },
+        serviceTypes: {
+            name: '',
+            description: ''
+        },
+        vendor: {
+            name: '',
+            type: ''
+        },
+        pop: {
+            name: '',
+            owner: 0,
+            extra_info: ''
+        },
+        address: {
+            flat: '',
+            floor: '',
+            holding: '',
+            street: '',
+            area: '',
+            thana: '',
+            district: '',
+            client_id: '',
+            service_id: '',
+            vendor_id: '',
+            extra_info: ''
+        },
+        contact: {
+            name: '',
+            designation: '',
+            type: '',
+            phone1: '',
+            phone2: '',
+            phone3: '',
+            client_id: 0,
+            service_id: 0,
+            vendor_id: 0
+        }
+    })
 
+    const vendorTypes = [{
+        id: 1,
+        type: 'LSP'
+    },
+    {
+        id: 2,
+        type: 'ISP'
+    },
+    {
+        id: 3,
+        type: 'NTTN'
+    }]
+
+    const contactTypes = [{
+        id: 1,
+        type: 'Admin'
+    },
+    {
+        id: 2,
+        type: 'Technical'
+    },
+    {
+        id: 3,
+        type: 'Billing'
+    }]
 
     const props = defineProps({
         viewName: {
@@ -89,16 +150,84 @@
     }
 
     const submitForm = async () => {
-        let formData
-        if (props.viewName === 'Services') {
-            formData = serviceData.value
+        if (props.viewName === 'Addresses') {
+            if (formData.value.address.client_id) {
+                formData.value.address.service_id = undefined
+                formData.value.address.vendor_id = undefined
+            }
+            else if (formData.value.address.service_id) {
+                formData.value.address.client_id = undefined
+                formData.value.address.vendor_id = undefined
+            }
+            else if (formData.value.address.vendor_id) {
+                formData.value.address.client_id = undefined
+                formData.value.address.service_id = undefined
+            }
+            else {
+                notification.value.message = "You must fill one of 'Clients', 'Services' or 'Vendors'"
+                notification.value.messageType = 'Warning'
+                emit('showNotification', notification.value.message, notification.value.messageType)
+                return
+            }
         }
-        else if (props.viewName === 'Clients') {
-            formData = clientData.value
+
+        if (props.viewName === 'Contacts') {
+            if (formData.value.contact.client_id) {
+                formData.value.contact.service_id = undefined
+                formData.value.contact.vendor_id = undefined
+            }
+            else if (formData.value.contact.service_id) {
+                formData.value.contact.client_id = undefined
+                formData.value.contact.vendor_id = undefined
+            }
+            else if (formData.value.contact.vendor_id) {
+                formData.value.contact.client_id = undefined
+                formData.value.contact.service_id = undefined
+            }
+            else {
+                notification.value.message = "You must fill one of 'Clients', 'Services' or 'Vendors'"
+                notification.value.messageType = 'Warning'
+                emit('showNotification', notification.value.message, notification.value.messageType)
+                return
+            }
+
+            if (!formData.value.contact.phone2) {
+                formData.value.contact.phone2 = undefined
+            }
+            if (!formData.value.contact.phone3) {
+                formData.value.contact.phone3 = undefined
+            }
+        }
+
+        let requestBody
+        switch (props.viewName) {
+            case 'Clients':
+                requestBody = formData.value.client
+                break;
+            case 'Services':
+                requestBody = formData.value.service
+                break;
+            case 'Service Types':
+                requestBody = formData.value.serviceTypes
+                break;
+            case 'Vendors':
+                requestBody = formData.value.vendor
+                break;
+            case 'Pops':
+                requestBody = formData.value.pop
+                break;
+            case 'Addresses':
+                requestBody = formData.value.address
+                break;
+            case 'Contacts':
+                requestBody = formData.value.contact
+                break;
+            default:
+                break;
         }
 
         const apiEndpoint = createApiUrl({ view: props.viewName, action: props.actionName })
-        const request = createRequest('POST', formData)
+        const request = createRequest('POST', requestBody)
         try {
             const response = await fetch(apiEndpoint, request)
 
@@ -110,7 +239,7 @@
             else {
                 const data = await response.json()
                 console.error(data)
-                message.value = 'API error'
+                message.value = `API error: ${data.detail}`
                 messageType.value = 'Error'
                 emit('showNotification', message.value, messageType.value)
             }
@@ -130,10 +259,9 @@
     <div class="data-form">
         <form @submit.prevent="handleSubmit">
             <div class="form-fields">
-                <ClientForm v-if="props.viewName === 'Clients'" :view-name="props.viewName" :action-name="props.actionName"
-                    :client-types="clientTypes" v-model="clientData" />
-                <ServiceForm v-if="props.viewName === 'Services'" :view-name="props.viewName"
-                    :action-name="props.actionName" :service-types="serviceTypes" v-model="serviceData" />
+                <Forms :view-name="props.viewName" :action-name="props.actionName" :client-types="clientTypes"
+                    :service-types="serviceTypes" :vendor-types="vendorTypes" :contact-types="contactTypes"
+                    v-model="formData" />
                 <button v-if="props.viewName !== ''" type="submit">Submit</button>
             </div>
             <div>
@@ -145,35 +273,5 @@
 </template>
 
 <style scoped>
-    /* Form style */
-    .data-form {
-        width: 30%;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .form-fields {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .submit-buttons {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-
-    .form-fields input,
-    select,
-    button {
-        box-sizing: border-box;
-        width: 100%;
-        display: block;
-        padding: 5px;
-        margin: 3px;
-    }
-
-    .form-fields button {
-        margin-top: 10px;
-    }
+    @import "@/assets/form.css";
 </style>
