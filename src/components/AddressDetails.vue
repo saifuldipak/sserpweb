717bc09b7d5d2c2320f4003f165e5e72d4398e43
Saiful Islam <saiful.kbtel@gmail.com>
2024-02-08 15:04:03 +0600

    Fix: add template for views other than 'Addresses'

--- src/components/AddressDetails.vue
+++ src/components/AddressDetails.vue
@@ -1,16 +1,31 @@
 <script setup>
     const props = defineProps({
-        addresses: Array
+        viewName: {
+            type: String,
+            required: true
+        },
+        itemDetails: {
+            type: Object,
+            required: true
+        }
     })
 </script>
 <template>
-    <ul v-for="address in props.addresses" :key="address.id">
-        <li>Flat: {{ address.flat }}</li>
-        <li>Floor: {{ address.floor }}</li>
-        <li>Holding: {{ address.holding }}</li>
-        <li>Street: {{ address.street }}</li>
-        <li>Area: {{ address.area }}</li>
-        <li>Thana: {{ address.thana }}</li>
-        <li>District: {{ address.district }}</li>
-    </ul>
+    <div v-if="props.viewName === 'Addresses'">
+        <li v-if="props.itemDetails.client_id">Client: {{ props.itemDetails.clients.name }}</li>
+        <li v-else-if="props.itemDetails.service_id">Service: {{ props.itemDetails.services.name }}</li>
+        <li v-else-if="props.itemDetails.vendor_id">Vendor: {{ props.itemDetails.vendors.name }}</li>
+        <li>Flat: {{ props.itemDetails.flat }}</li>
+        <li>Floor: {{ props.itemDetails.floor }}</li>
+        <li>Holding: {{ props.itemDetails.holding }}</li>
+        <li>Street: {{ props.itemDetails.street }}</li>
+        <li>Area: {{ props.itemDetails.area }}</li>
+        <li>Thana: {{ props.itemDetails.thana }}</li>
+        <li>District: {{ props.itemDetails.district }}</li>
+    </div>
+    <div v-else>
+        Flat:{{ props.itemDetails.flat }},Floor:{{ props.itemDetails.floor }},Holding:{{ props.itemDetails.holding
+        }},Street:{{
+    props.itemDetails.street }},Area:{{ props.itemDetails.area }}
+    </div>
 </template>
