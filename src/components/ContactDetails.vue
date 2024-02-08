ac20c82407910423ad8c47578546c416511f2f96
Saiful Islam <saiful.kbtel@gmail.com>
2024-02-08 15:04:18 +0600

    Fix: add template for views other than 'Contacts'

--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -1,16 +1,30 @@
 <script setup>
     const props = defineProps({
-        contacts: Array
+        viewName: {
+            type: String,
+            required: true
+        },
+        itemDetails: {
+            type: Object,
+            required: true
+        }
     })
 </script>
 
 <template>
-    <ul v-for="contact in props.contacts" :key="contact.id">
-        <li>Name: {{ contact.name }} </li>
-        <li>Designation: {{ contact.designation }}</li>
-        <li>Contact Type: {{ contact.type }}</li>
-        <li>Phone1: {{ contact.phone1 }}</li>
-        <li v-if="contact.phone2">Phone1: {{ contact.phone2 }}</li>
-        <li v-if="contact.phone3">Phone3: {{ contact.phone3 }}</li>
-    </ul>
+    <div v-if="props.viewName === 'Contacts'">
+        <li v-if="props.itemDetails.client_id">Client: {{ props.itemDetails.clients.name }}</li>
+        <li v-else-if="props.itemDetails.service_id">Service: {{ props.itemDetails.services.point }}</li>
+        <li v-else-if="props.itemDetails.vendor_id">Vendor: {{ props.itemDetails.vendors.name }}</li>
+        <li>Name: {{ props.itemDetails.name }}</li>
+        <li>Designation: {{ props.itemDetails.designation }}</li>
+        <li>Type: {{ props.itemDetails.type }}</li>
+        <li>Phone1: {{ props.itemDetails.phone1 }}</li>
+        <li>Phone2: {{ props.itemDetails.phone2 }}</li>
+        <li>Phone3: {{ props.itemDetails.phone3 }}</li>
+    </div>
+    <div v-else>
+        <li>{{ props.itemDetails.name }}, {{ props.itemDetails.designation }}, {{
+            props.itemDetails.phone1 }}</li>
+    </div>
 </template>
