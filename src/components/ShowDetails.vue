a43b7eb613c2524b06cd21955ac6f7dce2a0dda4
Saiful Islam <saiful.kbtel@gmail.com>
2024-03-18 10:17:54 +0600

    Fix: show details of all views

--- src/components/ShowDetails.vue
+++ src/components/ShowDetails.vue
@@ -1,95 +1,175 @@
 <script setup>
-    import AddressDetails from './AddressDetails.vue';
-    import ContactDetails from './ContactDetails.vue';
+    import AddressDetails from "./AddressDetails.vue";
+    import ContactDetails from "./ContactDetails.vue";
+
+    const viewList = ["Clients", "Services", "Vendors"];
 
     const props = defineProps({
         viewName: {
             type: String,
-            required: true
+            required: true,
         },
         itemDetails: {
             type: Object,
-            required: true
-        }
-    })
+            required: true,
+        },
+    });
 
-    const emit = defineEmits(['closeComponent', 'searchItem'])
+    const emit = defineEmits(["closeComponent", "searchItem"]);
 </script>
 
 <template>
-    <button @click="emit('closeComponent')">X</button>
-    <ul v-if="props.viewName === 'Clients'" class="item-details">
-        <li>Name: {{ props.itemDetails.name }}</li>
-        <li>Type: {{ props.itemDetails.client_types.name }}</li>
-        <li>Services:
-            <a href="#" @click="emit('searchItem', 'Services', `client_name:${props.itemDetails.name}`)">
-                <b>{{ props.itemDetails.services.length }}</b>
-            </a>
-        </li>
-        <li>Contacts:
-            <ul>
-                <li v-for="contact in props.itemDetails.contacts" :key="contact.id">
-                    <ContactDetails :item-details="contact" :view-name="props.viewName" />
-                </li>
-            </ul>
-        </li>
-        <li>Addresses:
-            <ul>
-                <li v-for="address in props.itemDetails.addresses" :key="address.id">
-                    <AddressDetails :item-details="address" :view-name="props.viewName" />
-                </li>
-            </ul>
-        </li>
-    </ul>
-    <ul v-if="props.viewName === 'Services'" class="item-details">
-        <li>Client name: {{ props.itemDetails.clients.name }}</li>
-        <li>Service point: {{ props.itemDetails.point }}</li>
-        <li>Service type: {{ props.itemDetails.service_types.name }}</li>
-        <li>Bandwidth: {{ props.itemDetails.bandwidth }}Mbps</li>
-        <li>Pop: {{ props.itemDetails.pops.name }}</li>
-        <li>Contacts:
-            <ul>
-                <li v-for="contact in props.itemDetails.contacts" :key="contact.id">
-                    <ContactDetails :item-details="contact" :view-name="props.viewName" />
-                </li>
-            </ul>
-        </li>
-        <li>Addresses:
-            <ul>
-                <li v-for="address in props.itemDetails.addresses" :key="address.id">
-                    <AddressDetails :item-details="address" :view-name="props.viewName" />
-                </li>
-            </ul>
-        </li>
-    </ul>
-    <ul v-if="props.viewName === 'Service Types'" class="item-details">
-        <li>Id: {{ props.itemDetails.id }}</li>
-        <li>Service type: {{ props.itemDetails.name }}</li>
-        <li>Description: {{ props.itemDetails.description }}</li>
+    <div class="main-container">
+        <div class="button-container">
+            <button @click="emit('closeComponent')">X</button>
+        </div>
+        <div class="data-container">
+            <!-- Heading -->
+            <div class="heading-container">
+                <!--Main heading -->
+                <div v-if="props.viewName === 'Services'" id="heading">{{ props.itemDetails.point }}</div>
+                <div v-else-if="props.viewName === 'Addresses'" id="heading">
+                    <span v-if="props.itemDetails.clients">{{ props.itemDetails.clients.name }}</span>
+                    <span v-else-if="props.itemDetails.services">{{ props.itemDetails.services.name }}</span>
+                    <span v-else-if="props.itemDetails.vendors">{{ props.itemDetails.vendors.name }}</span>
+                </div>
+                <div v-else id="heading">{{ props.itemDetails.name }}</div>
+                <!-- Sub heading-->
+                <div v-if="props.viewName === 'Clients'" id="sub-heading">{{ props.itemDetails.client_types.name }}</div>
+                <div v-else-if="props.viewName === 'Services'" id="sub-heading">{{ props.itemDetails.clients.name }}</div>
+                <div v-else-if="props.viewName === 'Service Types'" id="sub-heading">{{ props.itemDetails.description }}</div>
+                <div v-else-if="props.viewName === 'Vendors'" id="sub-heading">{{ props.itemDetails.type }}</div>
+                <div v-else-if="props.viewName === 'Pops'" id="sub-heading">{{ props.itemDetails.vendors.name }}</div>
+                <div v-else-if="props.viewName === 'Contacts'" id="sub-heading">{{ props.itemDetails.designation }}</div>
+            </div>
 
-    </ul>
-    <ul v-if="props.viewName === 'Vendors'" class="item-details">
-        <li>Id: {{ props.itemDetails.id }}</li>
-        <li>Name: {{ props.itemDetails.name }}</li>
-        <li>Type: {{ props.itemDetails.type }}</li>
-    </ul>
-    <ul v-if="props.viewName === 'Pops'" class="item-details">
-        <li>Id: {{ props.itemDetails.id }}</li>
-        <li>Name: {{ props.itemDetails.name }}</li>
-        <li>Owner: {{ props.itemDetails.vendors.name }}</li>
-        <li>Extra info: {{ props.itemDetails.extra_info }}</li>
-    </ul>
-    <ul v-if="props.viewName === 'Addresses'" class="item-details">
-        <AddressDetails :item-details="itemDetails" :view-name="props.viewName" />
-    </ul>
-    <ul v-if="props.viewName === 'Contacts'" class="item-details">
-        <ContactDetails :item-details="itemDetails" :view-name="props.viewName" />
-    </ul>
+            <!-- Details -->
+            <div v-if="props.viewName === 'Clients'" id="details">
+                <em>Services ({{ props.itemDetails.services.length }})</em>
+                <ol>
+                    <li v-for="service in props.itemDetails.services" :key="service.id">{{ service.point }} ({{ service.extra_info }})</li>
+                </ol>
+            </div>
+            <div v-else-if="props.viewName === 'Services'" id="details">
+                <span><strong>Service:</strong> {{ props.itemDetails.service_types.name }}</span>
+                <span><strong>Bandwidth:</strong> {{ props.itemDetails.bandwidth }} Mbps</span>
+                <span><strong>Pop:</strong> {{ props.itemDetails.pops.name }}</span>
+            </div>
+            <div v-else-if="props.viewName === 'Contacts'" id="details">
+                <span v-if="props.itemDetails.clients"><strong>Client:</strong>{{ props.itemDetails.clients.name }}</span>
+                <span v-if="props.itemDetails.services"><strong>Service:</strong>{{ props.itemDetails.services.name }}</span>
+                <span v-if="props.itemDetails.vendors"><strong>Vendor:</strong>{{ props.itemDetails.vendors.name }}</span>
+                <span><strong>Contact Type:</strong> {{ props.itemDetails.type }}</span>
+                <span><strong>Phone1:</strong> {{ props.itemDetails.phone1 }}</span>
+            </div>
+            <div v-else-if="props.viewName === 'Addresses'" id="details">
+                <span><strong>Flat:</strong> {{ props.itemDetails.flat }}</span>
+                <span><strong>Floor:</strong> {{ props.itemDetails.floor }}</span>
+                <span><strong>Holding:</strong> {{ props.itemDetails.holding }}</span>
+                <span><strong>Street:</strong> {{ props.itemDetails.street }}</span>
+                <span><strong>Area:</strong> {{ props.itemDetails.area }}</span>
+                <span><strong>Thana:</strong> {{ props.itemDetails.thana }}</span>
+                <span><strong>District:</strong> {{ props.itemDetails.district }}</span>
+            </div>
+
+            <!-- Contacts & Addresses list -->
+            <div v-if="viewList.includes(props.viewName)" id="contacts">
+                <em>Contacts({{ props.itemDetails.contacts.length }})</em>
+                <ol>
+                    <li v-for="contact in props.itemDetails.contacts" :key="contact.id">
+                        <ContactDetails :item-details="contact" :view-name="viewName" />
+                    </li>
+                </ol>
+            </div>
+            <div v-if="viewList.includes(props.viewName)" id="addresses">
+                <em>Addresses({{ props.itemDetails.addresses.length }})</em>
+                <ol>
+                    <li v-for="address in props.itemDetails.addresses" :key="address.id">
+                        <AddressDetails :item-details="address" :view-name="viewName" />
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-    .item-details {
-        border: 1px solid grey;
+    .main-container {
+        width: 40%;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid lightgray;
+        border-radius: 3%;
+        box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.5);
+        justify-content: flex-start;
+    }
+
+    .button-container {
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .button-container button {
+        border-radius: 5px;
+        margin: 1%;
+        background: rgb(233, 228, 228);
+        border: none;
+    }
+
+    .data-container {
+        width: 100%;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        padding: 2%;
+    }
+
+    .data-container div:not(.heading-container) {
+        border-radius: 10px;
+        padding: 3%;
+        margin: 1%;
+    }
+
+    .heading-container {
+        margin: 0;
+        margin-bottom: 15px;
+        color: brown;
+        padding-left: 3%;
+    }
+
+    #heading,
+    #sub-heading {
+        padding: 0;
+        margin: 0;
+    }
+
+    #heading {
+        font-weight: bolder;
+        font-size: larger;
+    }
+
+    #sub-heading {
+        font-size: medium;
+    }
+
+    .data-container ol {
         margin-top: 0;
+    }
+
+    #contacts {
+        background-color: bisque;
+    }
+
+    #addresses {
+        background-color: lightgray;
+    }
+
+    #details {
+        overflow: auto;
+        display: flex;
+        flex-direction: column;
+        background-color: rgb(252, 208, 214);
     }
 </style>
