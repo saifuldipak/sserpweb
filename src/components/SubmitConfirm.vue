76a6927188a3db2ff5dba62ae03c93dcb5cc1a4b
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-26 17:36:00 +0600

    Fix: add style

--- src/components/SubmitConfirm.vue
+++ src/components/SubmitConfirm.vue
@@ -7,25 +7,40 @@
         actionName: {
             type: String,
             required: true,
-        }
+        },
+        itemName: {
+            type: String,
+        },
     });
 
-    const emit = defineEmits(['confirm', 'cancel'])
+    const emit = defineEmits(["confirm", "cancel"]);
 </script>
 
 <template>
-    <div class="confirm" v-if="show">
-        <div class="message" v-if="props.actionName === 'Add'">Are you sure you want to add?</div>
-        <div class="message" v-if="props.actionName === 'Modify'">Are you sure you want to modify?</div>
-        <div class="message" v-if="props.actionName === 'Delete'">Are you sure you want to delete?</div>
+    <div class="confirm-message-box" v-if="show">
+        <div class="message" v-if="props.actionName === 'Add'">Are you sure you want to add "{{ props.itemName }}" ?</div>
+        <div class="message" v-if="props.actionName === 'Modify'">Are you sure you want to modify "{{ props.itemName }}" ?</div>
+        <div class="message" v-if="props.actionName === 'Delete'">Are you sure you want to delete "{{ props.itemName }}" ?</div>
         <div class="button-section">
             <button @click="$emit('confirm')">Yes</button>
             <button @click="$emit('cancel')">No</button>
         </div>
     </div>
 </template>
-  
+
 <style scoped>
+    .confirm-message-box {
+        width: 100%;
+        position: absolute;
+        z-index: 1000;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: rgb(255, 255, 198);
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+        border-radius: 5px;
+        padding: 10px;
+    }
     .confirm {
         display: flex;
         flex-direction: column;
