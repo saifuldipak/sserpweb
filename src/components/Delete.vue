b6e8a4410ab074801225602b7a774ee11dfb662d
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-26 17:42:36 +0600

    Fix: remove item details display divs

--- src/components/Delete.vue
+++ src/components/Delete.vue
@@ -1,84 +1,75 @@
 <script setup>
-    import { ref } from 'vue';
-    import SubmitConfirm from './SubmitConfirm.vue'
-    import { createApiUrl, createRequest } from '@/functions.js';
-    import { notification } from '../store';
+    import { ref } from "vue";
+    import SubmitConfirm from "./SubmitConfirm.vue";
+    import { createApiUrl, createRequest, createNotificationMessage } from "@/functions.js";
+    import { notification } from "../store";
 
-    const dialogVisible = ref(true)
-    const actionName = ref('Delete')
+    const dialogVisible = ref(true);
+    const actionName = ref("Delete");
+    const hideMessageBox = ref(false);
 
     const props = defineProps({
         viewName: {
             type: String,
-            required: true
+            required: true,
         },
         itemData: {
             type: Object,
-            required: true
+            required: true,
         },
-    })
+    });
 
-    const emit = defineEmits(['cancel'])
+    const emit = defineEmits(["cancel", "showNotification"]);
 
-    const closeDialog = (type = '') => {
-        dialogVisible.value = false
+    const closeDialog = (type = "") => {
+        dialogVisible.value = false;
         if (type) {
-            emit('cancel')
+            emit("cancel");
         }
     };
 
     const deleteItem = async () => {
-        const apiEndpoint = createApiUrl({ view: props.viewName, action: actionName.value }) + '/' + props.itemData.id
-        const method = 'DELETE'
-        const request = createRequest(method)
+        const apiEndpoint = createApiUrl({ view: props.viewName, action: actionName.value }) + "/" + props.itemData.id;
+        const method = "DELETE";
+        const request = createRequest(method);
 
         try {
-            const response = await fetch(apiEndpoint, request)
+            const response = await fetch(apiEndpoint, request);
             if (response.status === 200) {
-                notification.value.message = `${props.viewName} deleted`
-                notification.value.type = 'Info'
+                notification.value.message = createNotificationMessage(props.viewName, actionName.value);
+                notification.value.type = "Info";
+                emit("showNotification");
+            } else {
+                const data = await response.json();
+                notification.value.message = data.detail;
+                notification.value.type = "Error";
+                emit("showNotification");
             }
-            else {
-                const data = await response.json()
-                notification.value.message = data.detail
-                notification.value.type = 'Error'
-            }
+        } catch (error) {
+            console.error(error);
+            notification.value.message = error.message;
+            notification.value.type = "Error";
+        } finally {
+            closeDialog();
         }
-        catch (error) {
-            console.error(error)
-            notification.value.message = error.message
-            notification.value.type = 'Error'
-        }
-        finally {
-            closeDialog()
-        }
-    }
+    };
 </script>
 
 <template>
-    <div v-if="props.viewName === 'Clients'">
-        Id: {{ props.itemData.id }}
-        Name: {{ props.itemData.name }}
+    <div class="message-box-container">
+        <SubmitConfirm
+            v-model:show="dialogVisible"
+            :action-name="actionName"
+            :item-name="props.itemData.name"
+            @confirm="deleteItem"
+            @cancel="closeDialog('return')"
+        />
     </div>
-    <div v-else-if="props.viewName === 'Services'">
-        Id: {{ props.itemData.id }}
-        Point: {{ props.itemData.point }}
-        Pop: {{ props.itemData.pops.name }}
-    </div>
-    <div v-else-if="props.viewName === 'Service Types'">
-        Id: {{ props.itemData.id }}
-        Service type: {{ props.itemData.name }}
-    </div>
-    <div v-else-if="props.viewName === 'Vendors'">
-        Id: {{ props.itemData.id }}
-        Name: {{ props.itemData.name }}
-        Type: {{ props.itemData.type }}
-    </div>
-    <div v-else-if="props.viewName === 'Pops'">
-        Id: {{ props.itemData.id }}
-        Name: {{ props.itemData.name }}
-        Type: {{ props.itemData.vendors.name }}
-    </div>
-    <SubmitConfirm v-model:show="dialogVisible" :action-name="actionName" @confirm="deleteItem"
-        @cancel="closeDialog('return')" />
 </template>
+
+<style scoped>
+    .message-box-container {
+        width: 30%;
+        position: relative;
+    }
+</style>
