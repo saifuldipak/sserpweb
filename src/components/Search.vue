e64bd45fb7efb8a6984ccf15df73de96414c0007
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-26 17:38:43 +0600

    Fix: remove checking searchString value

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,63 +1,52 @@
 <script setup>
-    import { ref, watch } from 'vue'
-    import { createApiUrl, createRequest } from '@/functions.js'
-    import { notification } from '../store';
+    import { ref, watch } from "vue";
+    import { createApiUrl, createRequest } from "@/functions.js";
+    import { notification } from "../store";
 
-    const searchString = ref('')
-    const itemList = ref([])
+    const searchString = ref("");
+    const itemList = ref([]);
     const props = defineProps({
         viewName: {
             type: String,
-            required: true
-        }
-    })
+            required: true,
+        },
+    });
 
-    const emit = defineEmits(['showData', 'showNotification', 'showHelpMessage'])
+    const emit = defineEmits(["showData", "showNotification", "showHelpMessage"]);
 
     watch(props, () => {
-        searchString.value = ''
-    })
+        searchString.value = "";
+    });
 
     const handleSearch = async () => {
-        if (!searchString.value) {
-            notification.value.message = 'Must provide search parameter, please see help for details'
-            notification.value.type = 'Warning'
-            return
-        }
-
-        const apiEndpoint = createApiUrl({ view: props.viewName, action: 'search', searchString: searchString.value })
-        const request = createRequest('GET')
+        const apiEndpoint = createApiUrl({ view: props.viewName, action: "search", searchString: searchString.value });
+        const request = createRequest("GET");
         try {
-            const response = await fetch(apiEndpoint, request)
+            const response = await fetch(apiEndpoint, request);
             if (response.ok) {
-                itemList.value = await response.json()
-                emit('showData', itemList.value)
+                itemList.value = await response.json();
+                emit("showData", itemList.value);
+            } else {
+                const responseMessage = await response.json();
+                notification.value.message = responseMessage.detail;
+                notification.value.type = "Error";
+                itemList.value = [];
+                emit("showNotification");
             }
-            else {
-                const responseMessage = await response.json()
-                notification.value.message = responseMessage.detail
-                notification.value.type = 'Error'
-                itemList.value = []
-                emit('showData', itemList.value)
-            }
+        } catch (error) {
+            notification.value.message = error.message;
+            notification.value.type = "Error";
+            emit("showData", itemList.value);
         }
-        catch (error) {
-            notification.value.message = error.message
-            notification.value.type = 'Error'
-            emit('showData', itemList.value)
-        }
-    }
+    };
 </script>
 
 <template>
     <div class="search">
         <div class="search-box">
-            <span class="material-symbols-outlined search-icon">
-                search
-            </span>
+            <span class="material-symbols-outlined search-icon"> search </span>
             <form @submit.prevent="handleSearch">
-                <input name="search-input" type="text" placeholder="Enter text..." v-model="searchString" required
-                    autocomplete="on" />
+                <input name="search-input" type="text" :placeholder="`Search ${props.viewName}`" v-model="searchString" autocomplete="on" />
             </form>
         </div>
         <a href="#" @click="$emit('showHelpMessage')"><img src="@/components/icons/help.png" title="search help" /></a>
