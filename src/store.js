fee5e900ec5399bb8e9ab84da0b41e41b22e2ecd
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-26 17:24:19 +0600

    Fix: add formData object

--- src/store.js
+++ src/store.js
@@ -4,3 +4,59 @@ export const notification = ref({
     type: "",
     message: "",
 });
+
+export const formData = ref({
+    client: {
+        name: "",
+        client_type_id: "",
+    },
+    service: {
+        client_id: "",
+        point: "",
+        service_type_id: "",
+        bandwidth: "",
+        pop_id: "",
+        extra_info: "",
+    },
+    serviceTypes: {
+        name: "",
+        description: "",
+    },
+    vendor: {
+        name: "",
+        type: "",
+    },
+    pop: {
+        name: "",
+        owner: 0,
+        extra_info: "",
+    },
+    address: {
+        flat: "",
+        floor: "",
+        holding: "",
+        street: "",
+        area: "",
+        thana: "",
+        district: "",
+        client_id: "",
+        service_id: "",
+        vendor_id: "",
+        extra_info: "",
+    },
+    contact: {
+        name: "",
+        designation: "",
+        type: "",
+        phone1: "",
+        phone2: "",
+        phone3: "",
+        client_id: 0,
+        service_id: 0,
+        vendor_id: 0,
+    },
+    clientTypes: {
+        name: "",
+        description: "",
+    },
+});
