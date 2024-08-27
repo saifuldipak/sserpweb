b11499ecbc14229c970bab651f1b196d344df36d
Saiful Islam <saiful.kbtel@gmail.com>
2024-08-27 16:31:56 +0600

    add logic for view 'Account Managers' in template

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -2,30 +2,34 @@
     const props = defineProps({
         itemList: {
             type: Array,
-            required: true
+            required: true,
         },
         viewName: {
             type: String,
-            required: true
-        }
-    })
+            required: true,
+        },
+    });
 
-    const emit = defineEmits(['showDetails', 'modifyItem', 'deleteItem'])
+    const emit = defineEmits(["showDetails", "modifyItem", "deleteItem"]);
 </script>
 
 <template>
     <table v-if="props.itemList.length > 0">
         <tr v-for="item in props.itemList" :key="item.id">
             <!-- First column -->
-            <td v-if="props.viewName !== 'Services'">
-                <a href="#" @click="emit('showDetails', item)">{{ item.name }}</a>
-            </td>
-            <td v-else>
+            <td v-if="props.viewName === 'Services'">
                 <a href="#" @click="emit('showDetails', item)">{{ item.point }}</a>
             </td>
-            <td v-if="props.viewName === 'Addresses'">
+            <td v-else-if="props.viewName === 'Account Managers'">
+                <a href="#" @click="emit('showDetails', item.contacts)">{{ item.contacts.name }}</a>
+            </td>
+            <td v-else-if="props.viewName === 'Addresses'">
                 <a href="#" @click="emit('showDetails', item)">{{ item.area }}, {{ item.thana }}, {{ item.district }}</a>
             </td>
+            <td v-else>
+                <a href="#" @click="emit('showDetails', item)">{{ item.name }}</a>
+            </td>
+
             <!-- Second column -->
             <td v-if="props.viewName === 'Clients'">{{ item.client_types.name }}</td>
             <td v-else-if="props.viewName === 'Services'">{{ item.clients.name }}</td>
@@ -34,20 +38,19 @@
                 <div v-else-if="item.service_id">{{ item.services.point }}</div>
                 <div v-else-if="item.vendor_id">{{ item.vendors.name }}</div>
             </td>
+            <td v-else-if="props.viewName === 'Account Managers'">
+                <a href="#" @click="emit('showDetails', item.clients)">{{ item.clients.name }}</a>
+            </td>
             <!-- Third column -->
-            <td v-if="props.viewName === 'Pops'"> {{ item.vendors.name }}</td>
+            <td v-if="props.viewName === 'Pops'">{{ item.vendors.name }}</td>
             <!-- Fourth column -->
-            <td v-if="props.viewName === 'Pops'"> {{ item.extra_info }}</td>
+            <td v-if="props.viewName === 'Pops'">{{ item.extra_info }}</td>
             <!-- Fifth column -->
             <td class="button-section">
-                <a href="#" v-if="props.viewName !== 'Service Types'" class="modify"
-                    @click="$emit('modifyItem', item.id)"><span class="material-symbols-outlined modify-icon">
-                        edit
-                    </span></a>
-                <a href="#" class="delete" @click="$emit('deleteItem', item.id)"><span
-                        class="material-symbols-outlined delete-icon">
-                        delete
-                    </span></a>
+                <a href="#" v-if="props.viewName !== 'Service Types'" class="modify" @click="$emit('modifyItem', item.id)"
+                    ><span class="material-symbols-outlined modify-icon"> edit </span></a
+                >
+                <a href="#" class="delete" @click="$emit('deleteItem', item.id)"><span class="material-symbols-outlined delete-icon"> delete </span></a>
             </td>
         </tr>
     </table>
