54e813c37c79fceabce5392e2a90d2fb9bde0d5c
Saiful Islam <saiful.kbtel@gmail.com>
2024-09-08 16:47:10 +0600

    redesign Search.vue to search and show data in the same component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,105 +1,116 @@
 <script setup>
-    import { ref, watch } from "vue";
-    import { createApiUrl, createRequest } from "@/functions.js";
+    import { ref, watchEffect } from "vue";
+    import { createRequest } from "@/functions.js";
     import { notification } from "../store";
+    import { API_HOST } from "../config";
+    import InputSuggestion from "./InputSuggestion.vue";
 
-    const searchString = ref("");
-    const itemList = ref([]);
+    const clientDetails = ref([]);
+    const accountManagers = ref([]);
     const props = defineProps({
         viewName: {
             type: String,
             required: true,
         },
+        searchString: {
+            type: String,
+        },
     });
 
-    const emit = defineEmits(["showData", "showNotification", "showHelpMessage"]);
+    const emit = defineEmits(["showNotification", "changeView"]);
 
-    watch(props, () => {
-        searchString.value = "";
-    });
+    const searchItem = async (itemId, itemName) => {
+        let apiEndpoint;
+        accountManagers.value = [];
+        if (props.viewName === "Clients") {
+            apiEndpoint = API_HOST + "/clients?" + `client_id=${itemId}`;
+        } else if (props.viewName === "Account Managers") {
+            apiEndpoint = API_HOST + "/account_managers?" + `client_id=${itemId}`;
+        }
 
-    const handleSearch = async () => {
-        const apiEndpoint = createApiUrl({ view: props.viewName, action: "search", searchString: searchString.value });
         const request = createRequest("GET");
         try {
             const response = await fetch(apiEndpoint, request);
             if (response.ok) {
-                itemList.value = await response.json();
-                emit("showData", itemList.value);
+                if (props.viewName === "Clients") {
+                    clientDetails.value = await response.json();
+                } else if (props.viewName === "Account Managers") {
+                    accountManagers.value = await response.json();
+                }
             } else {
                 const responseMessage = await response.json();
                 notification.value.message = responseMessage.detail;
-                notification.value.type = "Error";
-                itemList.value = [];
+                notification.value.type = "Warning";
                 emit("showNotification");
             }
         } catch (error) {
             notification.value.message = error.message;
             notification.value.type = "Error";
-            emit("showData", itemList.value);
+            emit("showNotification");
         }
     };
+
+    watchEffect(() => {
+        if (props.viewName) {
+            clientDetails.value = [];
+            accountManagers.value = [];
+        }
+
+        if (props.searchString) {
+            searchItem(props.searchString);
+        }
+    });
 </script>
 
 <template>
-    <div class="search">
-        <div class="search-box">
-            <span class="material-symbols-outlined search-icon"> search </span>
-            <form @submit.prevent="handleSearch">
-                <input name="search-input" type="text" :placeholder="`Search ${props.viewName}`" v-model="searchString" autocomplete="on" />
-            </form>
+    <div class="form">
+        <div>Search</div>
+        <div class="form-fields">
+            <InputSuggestion :search-item="'client name'" :view-name="props.viewName" @selected-item="searchItem" />
         </div>
-        <a href="#" @click="$emit('showHelpMessage')"><img src="@/components/icons/help.png" title="search help" /></a>
+    </div>
+    <div v-if="clientDetails.length">
+        <div>{{ clientDetails[0].name }}</div>
+        <div>{{ clientDetails[0].client_types.name }}</div>
+        <div v-if="clientDetails[0].account_managers.length > 0">
+            <a href="#" @click="emit('changeView', 'Account Managers', clientDetails[0].id)"
+                >Account Managers ({{ clientDetails[0].account_managers.length }})</a
+            >
+        </div>
+        <div v-else>Account Managers ({{ clientDetails[0].account_managers.length }})</div>
+
+        <!--  <div>
+            <em>Addresses ({{ clientDetails[0].addresses.length }})</em>
+            <ol>
+                <li v-for="address in clientDetails[0].addresses" :key="address.id">{{ address.flat }}</li>
+            </ol>
+        </div>
+        <div>
+            <em>Contacts ({{ clientDetails[0].contacts.length }})</em>
+            <ol>
+                <li v-for="contact in clientDetails[0].contacts" :key="contact.id">{{ contact.name }}</li>
+            </ol>
+        </div>
+        <div>
+            <em>Services ({{ clientDetails[0].services.length }})</em>
+            <ol>
+                <li v-for="service in clientDetails[0].services" :key="service.id">{{ service.point }} ({{ service.extra_info }})</li>
+            </ol>
+        </div> -->
+    </div>
+    <div v-else-if="accountManagers.length > 0">
+        {{ accountManagers[0].clients.name }}
+        <ol>
+            <li v-for="accountManager in accountManagers" :key="accountManager.id">
+                <div>{{ accountManager.contacts.name }} ({{ accountManager.contacts.type }})</div>
+                <div>{{ accountManager.contacts.designation }}</div>
+                <div>{{ accountManager.contacts.phone1 }}</div>
+                <div>{{ accountManager.contacts.email }}</div>
+            </li>
+        </ol>
     </div>
 </template>
 
 <style>
-    .search {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-end;
-    }
-
-    .search a {
-        display: block;
-        padding-left: 5px;
-    }
-
-    .help-icon {
-        margin: 0;
-        background-color: lightgrey;
-    }
-
-    .search-box {
-        display: flex;
-        flex-direction: row;
-        border: 1px solid lightgrey;
-        border-radius: 5px;
-    }
-
-    .search-box form {
-        width: 100%;
-    }
-
-    .search-box input {
-        border: none;
-        width: 100px;
-    }
-
-    .search-box input:focus {
-        outline: none;
-    }
-
-    .search-icon {
-        background-color: lightgrey;
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-    }
-
-    @media only screen and (min-width: 768px) {
-        .search-box input {
-            width: 300px;
-        }
-    }
+    @import "@/assets/form.css";
 </style>
