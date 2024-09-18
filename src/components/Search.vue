8f8390d594469d41e525a3c9ec4c05b3ee8eaa62
Saiful Islam <saiful.kbtel@gmail.com>
2024-09-18 15:42:40 +0600

    redesign Search.vue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,15 +1,26 @@
 <script setup>
-    import { ref, watchEffect } from "vue";
-    import { createRequest } from "@/functions.js";
+    import { onMounted, ref, watchEffect } from "vue";
+    import { callApi } from "@/functions.js";
     import { notification } from "../store";
-    import { API_HOST } from "../config";
-    import InputSuggestion from "./InputSuggestion.vue";
+    import SearchResults from "./SearchResults.vue";
+    import ShowDetails from "./ShowDetails.vue";
 
-    const clientId = ref(0);
-    const clientType = ref("");
-    const showSearchField = ref(false);
-    const searchResult = ref([]);
-    const accountManagers = ref([]);
+    const showSearchResults = ref(false);
+    const itemDetails = ref({});
+    const showDetails = ref(false);
+    const serviceNameFieldClass = ref("");
+    const clientTypeFieldClass = ref("");
+    const clientNameFieldClass = ref("");
+    const serviceName = ref("");
+    const showServiceNameField = ref(false);
+    const showClientNameField = ref(false);
+    const showClientTypeField = ref(false);
+    const clientTypesNotFound = ref(false);
+    const clientName = ref("");
+    const message = ref("");
+    const selectedClientType = ref("");
+    const clientTypes = ref([]);
+    const searchResults = ref([]);
     const props = defineProps({
         viewName: {
             type: String,
@@ -22,11 +33,149 @@
 
     const emit = defineEmits(["showNotification", "changeView", "logout"]);
 
+    const removeAllFields = () => {
+        showClientNameField.value = false;
+        showClientTypeField.value = false;
+        showServiceNameField.value = false;
+        clientName.value = "";
+        serviceName.value = "";
+        selectedClientType.value = "";
+    };
+
+    onMounted(async () => {
+        if (props.viewName === "Clients") {
+            try {
+                clientTypes.value = await callApi("GET", "/client/types");
+            } catch (error) {
+                if (error.message === "Not found") {
+                    clientTypesNotFound.value = true;
+                } else {
+                    notification.value.message = error.message;
+                    notification.value.type = "Error";
+                    emit("showNotification");
+                }
+            }
+        }
+    });
+
+    watchEffect(() => {
+        removeAllFields();
+        searchResults.value = [];
+        if (props.viewName === "Clients") {
+            showClientNameField.value = true;
+            clientNameFieldClass.value = "position1";
+            clientTypeFieldClass.value = "position3";
+        } else if (props.viewName === "Services") {
+            showServiceNameField.value = true;
+            serviceNameFieldClass.value = "position1";
+            clientNameFieldClass.value = "position3";
+        }
+    });
+
+    const toggleField = () => {
+        if (props.viewName === "Clients") {
+            if (showClientTypeField.value) {
+                showClientTypeField.value = false;
+                selectedClientType.value = "";
+            } else {
+                showClientTypeField.value = true;
+            }
+        } else if (props.viewName === "Services") {
+            if (showClientNameField.value) {
+                showClientNameField.value = false;
+            } else {
+                showClientNameField.value = true;
+            }
+        }
+    };
+
     const searchItem = async () => {
+        let resource, queryString;
+        if (props.viewName === "Clients") {
+            resource = "/clients";
+            queryString = `client_name=${clientName.value}&client_type=${selectedClientType.value}`;
+        }
+
+        try {
+            searchResults.value = await callApi("GET", resource, queryString);
+            if (searchResults.value.length > 0) {
+                showSearchResults.value = true;
+            }
+        } catch (error) {
+            if (error.message === "Not found") {
+                message.value = "Nothing found";
+            } else {
+                notification.value.message = error.message;
+                notification.value.type = "Error";
+                emit("showNotification");
+            }
+        }
+    };
+
+    const viewDetails = (item) => {
+        itemDetails.value = item;
+        showSearchResults.value = false;
+        showDetails.value = true;
+    };
+
+    const closeDetails = () => {
+        showDetails.value = false;
+        showSearchResults.value = true;
+    };
+
+    /* const handleSelect = async (apiResource, queryParameter) => {
+        if (apiResource === "clients") {
+            clients.value = [];
+        }
+        await searchItem(apiResource, queryParameter, selectedClientType.value);
+    }; */
+
+    /* 
+    let timeout = null;
+    const handleInput = async () => {
+        clearTimeout(timeout);
+        if (props.viewName === "Clients") {
+            if (clientName.value || selectedClientType.value) {
+            }
+            timeout = setTimeout(async () => {
+                await searchItem(apiEndpoint);
+            }, 1500);
+        }
+    }; */
+
+    /* const searchItem = async () => {
+        const request = createRequest("GET");
+        try {
+            const response = await fetch(apiEndpoint.value, request);
+            if (response.status === 200) {
+                message.value = "";
+                if (props.viewName === "Client Types") {
+                    clientTypes.value = await response.json();
+                } else if (props.viewName === "Clients") {
+                    clients.value = await response.json();
+                }
+            } else if (response.status === 401) {
+                emit("logout");
+            } else if (response.status === 404) {
+                message.value = "Nothing found";
+            } else {
+                const data = await response.json();
+                throw new Error(data.detail);
+            }
+        } catch (error) {
+            console.error(error);
+            notification.value.message = error.message;
+            notification.value.type = "Error";
+            emit("showNotification");
+        }
+    }; */
+    /* const searchItem = async () => {
         let apiEndpoint;
         accountManagers.value = [];
+        searchResult.value = [];
         if (props.viewName === "Clients") {
-            apiEndpoint = API_HOST + "/clients?" + `client_id=${clientId.value}` + "&" + `client_type=${clientType.value}`;
+            apiEndpoint =
+                API_HOST + "/clients?" + `client_id=${clientId.value}` + "&" + `client_type=${clientType.value}` + "&" + `page_size=${pageSize.value}`;
         } else if (props.viewName === "Account Managers") {
             apiEndpoint = API_HOST + "/account_managers?" + `client_id=${itemId}`;
         }
@@ -61,45 +210,71 @@
         if (props.searchString) {
             searchItem(props.searchString);
         }
-    });
+    }); */
 
-    const toggleSearchField = () => {
-        if (showSearchField.value) {
-            showSearchField.value = false;
-        } else {
-            showSearchField.value = true;
-        }
-    };
-
-    const setItemValue = (searchItem, itemId, itemName) => {
+    /* const setItemValue = (searchItem, itemId, itemName) => {
         if (searchItem === "client name") {
             clientId.value = itemId;
         } else if (searchItem === "client type") {
             clientType.value = itemName;
         }
-    };
+    }; */
 </script>
 
 <template>
-    <div class="form">
+    <!-- Search form -->
+    <form class="form" @submit.prevent="searchItem">
         <div>Search</div>
-        <form @submit.prevent="searchItem">
-            <div class="first-search-input">
-                <div class="item1">
-                    <InputSuggestion :search-item="'client name'" :view-name="props.viewName" @selected-item="setItemValue" @logout="emit('logout')" />
-                </div>
-                <div class="item2">
-                    <span class="material-symbols-outlined tooltip" title="more search options" @click="toggleSearchField">add_circle</span>
-                </div>
-                <div v-if="showSearchField" class="item1">
-                    <InputSuggestion :search-item="'client type'" :view-name="props.viewName" @selected-item="setItemValue" />
-                    <input type="submit" value="Search" />
-                </div>
+        <div class="container">
+            <div v-if="showClientNameField" :class="clientNameFieldClass">
+                <input type="text" v-model="clientName" placeholder="client name" />
             </div>
-        </form>
-    </div>
-    <div v-if="searchResult.length">
-        <div v-if="searchResult.length === 1">
+            <div v-if="showServiceNameField" :class="serviceNameFieldClass">
+                <input type="text" v-model="serviceName" placeholder="service name" />
+            </div>
+            <div class="position2">
+                <span class="material-symbols-outlined tooltip" title="more search options" @click="toggleField">add_circle</span>
+            </div>
+
+            <div v-if="showClientTypeField" :class="clientTypeFieldClass">
+                <select v-model="selectedClientType">
+                    <option disabled value="">client type</option>
+                    <option v-for="clientType in clientTypes" :key="clientType.id" :value="clientType.name">
+                        {{ clientType.name }}
+                    </option>
+                </select>
+                <div v-if="clientTypesNotFound">Please add client types</div>
+            </div>
+            <div class="position4"><input type="submit" value="Search" /></div>
+        </div>
+    </form>
+    <div v-if="message" class="message">{{ message }}</div>
+
+    <SearchResults v-if="showSearchResults" :searchResults="searchResults" :viewName="props.viewName" @show-details="viewDetails" />
+    <ShowDetails v-if="showDetails" :item-details="itemDetails" :viewName="props.viewName" @close="closeDetails" />
+
+    <!-- Search results -->
+    <!-- <div v-if="searchResults.length > 0">
+        <ol>
+            <li v-for="item in searchResults" :key="item.id">
+                <div v-if="props.viewName === 'Clients'">
+                    <a href="#" @click="showDetails(item.id)">{{ item.name }}, {{ item.client_types.name }}</a>
+                </div>
+                <div v-else>{{ item.name }}, {{ item.client_types.name }}</div>
+            </li>
+        </ol>
+    </div> -->
+
+    <!-- <div class="grid-container">
+        <div class="item1">Item 1</div>
+        <div class="item2">Item 2</div>
+        <div class="item3">Item 3 (spanning only the first column)</div>
+    </div> -->
+
+    <!-- Search results -->
+
+    <!-- <div v-if="searchResults.length">
+        <div v-if="searchResults.length === 1">
             <div v-if="props.viewName === 'Clients'">
                 <div>{{ searchResult[0].name }}</div>
                 <div>{{ searchResult[0].client_types.name }}</div>
@@ -110,16 +285,18 @@
                 </div>
                 <div v-else>Account Managers ({{ searchResult[0].account_managers.length }})</div>
             </div>
-        </div>
-        <div v-else-if="searchResult.length > 0">
-            <ol>
-                <li v-for="item in searchResult" :key="item.id">
-                    <div v-if="props.viewName === 'Clients'">{{ item.name }}, {{ item.client_types.name }}</div>
-                </li>
-            </ol>
-        </div>
+        </div> -->
+
+    <!--  <div v-if="clients.length > 0">
+        <ol>
+            <li v-for="client in clients" :key="client.id">
+                <a href="#" @click="showDetails(client.id)">{{ client.name }}, {{ client.client_types.name }}</a>
+            </li>
+        </ol>
     </div>
-    <div v-else-if="accountManagers.length > 0">
+    <div v-if="message">{{ message }}</div> -->
+
+    <!-- <div v-if="accountManagers.length > 0">
         {{ accountManagers[0].clients.name }}
         <ol>
             <li v-for="accountManager in accountManagers" :key="accountManager.id">
@@ -129,19 +306,36 @@
                 <div>{{ accountManager.contacts.email }}</div>
             </li>
         </ol>
-    </div>
+    </div> -->
 </template>
 
 <style>
     @import "@/assets/form.css";
-    .first-search-input {
+    .container {
         display: grid;
         grid-template-columns: 6fr 1fr;
+        /* grid-template-rows: auto auto; */
     }
 
-    .item2 {
-        display: grid;
-        place-items: center;
+    .position1 {
+        grid-column: 1;
+        grid-row: 1;
+    }
+
+    .position2 {
+        padding: 5px;
+        grid-column: 2;
+        grid-row: 1;
+    }
+
+    .position3 {
+        grid-column: 1;
+        grid-row: 2;
+    }
+
+    .position4 {
+        grid-column: 1;
+        grid-row: 3;
     }
 
     .tooltip {
